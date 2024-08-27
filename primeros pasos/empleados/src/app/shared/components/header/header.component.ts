64f58765724005d4616678e8043cc5381b58b66a
Felipe Gómez <feligomez160@gmail.com>
2024-08-27 18:31:05 -0500

    yulius10
    front

--- primeros pasos/empleados/src/app/shared/components/header/header.component.ts
+++ primeros pasos/empleados/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent  implements OnInit {
+
+  @Input() title!: string; 
+  @Input() showMenu!: boolean; 
+  @Input() backButton!: string; 
+
 
   constructor() { }
 
