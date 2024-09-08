1d587874662e7daa726a1a0dbba3d35e290b6ec9
Felipe Gómez <feligomez160@gmail.com>
2024-09-08 18:53:22 -0500

    yulius10
    se agrega auth

--- primeros pasos/empleados/src/app/pages/auth/auth.page.ts
+++ primeros pasos/empleados/src/app/pages/auth/auth.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,19 +11,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AuthPage implements OnInit {
 
+  firebaseService = inject(FirebaseService);
+
   form = new FormGroup({
     email: new FormControl('',[Validators.required,Validators.email]),
-    password: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required])
   });
-
   constructor() { }
 
   ngOnInit() {
 
   }
 
-  submit(){
+  async submit(){
     if(this.form.valid){
+      this.firebaseService.signIn(this.form.value as User)
+        .then(resp => {
+          console.log('__________',resp)
+        });
       console.log(this.form.value);
     }
     else{
--- primeros pasos/empleados/src/app/services/firebase.service.ts
+++ primeros pasos/empleados/src/app/services/firebase.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendPasswordResetEmail  } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { User } from '../models/user.model';
+import { doc, getDoc, getFirestore, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
+  auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore);
+
+  getAuth() {
+    return getAuth();
+  }
+
+  signIn(user: User) {
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  
 }
